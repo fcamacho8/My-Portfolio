feb2100a026652db929e93850e147e9ecfedf656
FCTESLA <fcamacho888@gmail.com>
2020-07-21 00:36:50 -0700

    navbar linking routes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,29 +1,37 @@
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 
 function Navbar() {
-    
+
 
     return (
 
         <nav class="navbar transparent navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="#">Home</a>
+            
+            <a class="navbar-brand" href="/home">Home</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">About me <span class="sr-only">(current)</span></a>
+                    <li class="nav-item">
+                    <Link to="/home" className="nav-link">
+                            About me
+                        </Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Portfolio</a>
+                        <Link to="/portfolio" className="nav-link">
+                            Portfolio
+                        </Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contact me</a>
+                        <Link to="/contact" className="nav-link">
+                            Contact me
+                        </Link>
                     </li>
-                   
+
                 </ul>
             </div>
         </nav>
